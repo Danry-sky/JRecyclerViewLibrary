6549afa916c1582cf71c7c3a02bf9f61c146445a
wuxubaiyang <wuxubaiyang@gmail.com>
2016-05-26 11:17:54 +0800

    我也不知道更新了些啥，反正就是更新了

--- simple/src/main/java/com/jtech/jrecyclerviewlibrary/TestActivity.java
+++ simple/src/main/java/com/jtech/jrecyclerviewlibrary/TestActivity.java
@@ -1,10 +1,9 @@
 package com.jtech.jrecyclerviewlibrary;
 
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Toast;
